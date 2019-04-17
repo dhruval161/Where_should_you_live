f7022c753e1db7189d14b8c8733e018d0f81e820
Dhruval <dhruval161@gmail.com>
2019-04-18 04:39:11 +0530

    comming soon added

--- assets/js/app.js
+++ assets/js/app.js
@@ -403,7 +403,7 @@ $(function () {
     $( function() {
         // Add background image
         //$.backstretch('../img/nature.jpg');
-        var endDate = "December  27, 2019 15:03:25";
+        var endDate = "April 22, 2019 15:03:25";
         $('.countdown.simple').countdown({ date: endDate });
         $('.countdown.styled').countdown({
             date: endDate,
